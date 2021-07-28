79439a3e2a6d721c166b4b11cea4719fda2c2813
hackme199 <hackme199@gmail.com>
2021-07-28 18:07:04 +0530

    Signing transactions

--- main.js
+++ main.js
@@ -1,114 +1,21 @@
-const SHA256 = require('crypto-js/sha256');
+const {Blockchain, Transaction} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-class Transaction{
-    constructor(fromaddress, toAddress, amount){
-        this.fromaddress = fromaddress;
-        this.toAddress = toAddress;
-        this.amount = amount;
-    }
-}
-
-class Block{
-    constructor(timestamp, transactions, previousHash = ''){
-        this. timestamp = timestamp;
-        this.transactions = transactions;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-        this.nonce = 0;
-    }
-
-    calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
-    }
-
-    mineBlock(difficulty){
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-
-        console.log("Block mined : " + this.hash);
-    }
-}
-
-class Blockchain{
-    constructor(){
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 2;
-        this.pendingTransactions = [];
-        this.miningReward = 100;
-    }
-
-    createGenesisBlock(){
-        return new Block("28/07/2021", "Genesis Block", "0");
-    }
-
-    getLatestBlock(){
-        return this.chain[this.chain.length-1];
-    }
-
-    minePendingTransactions(miningRewardAddress){
-        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
-        block.mineBlock(this.difficulty);
-
-        console.log("Block Successfully mined");
-
-        this.chain.push(block);
-
-        this.pendingTransactions = [
-            new Transaction(null, miningRewardAddress, this.miningReward)
-        ];
-    }
-
-    createTransaction(transaction){
-        this.pendingTransactions.push(transaction);
-    }
-
-    getBalanceOfAddress(address){
-        let balance = 0;
-
-        for(const block of this.chain){
-            for(const trans of block.transactions){
-                if(trans.fromaddress === address){
-                    balance -= trans.amount;
-                }
-
-                if(trans.toAddress === address){
-                    balance += trans.amount;
-                }
-            }
-        }
-
-        return balance;
-    }
-
-    isChainValid(){
-        for(let i = 1; i < this.chain.length; i++){
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i-1];
-
-            if(currentBlock.hash !== currentBlock.calculateHash()){
-                return false;
-            }
-
-            if(currentBlock.previousHash !== previousBlock.hash){
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
+const myKey = ec.keyFromPrivate('c1c9070ea474a93475f1278150be7fc672841820f581a98e76d9489f73fbbc2d');
+const myWalletAddress = myKey.getPublic('hex');
 
 let hackmecoin = new Blockchain();
 
-hackmecoin.createTransaction(new Transaction('address1', 'address2', 100));
-hackmecoin.createTransaction(new Transaction('address2', 'address1', 50));
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+hackmecoin.addTransaction(tx1);
+
+// hackmecoin.createTransaction(new Transaction('address1', 'address2', 100));
+// hackmecoin.createTransaction(new Transaction('address2', 'address1', 50));
 
 console.log('\nStarting the miner...');
-hackmecoin.minePendingTransactions('hackme-address');
+hackmecoin.minePendingTransactions(myWalletAddress);
+//hackmecoin.minePendingTransactions(myWalletAddress);
 
-// console.log('\n Starting the miner again...');
-// hackmecoin.minePendingTransactions('hackme-address');
-
-console.log('\nBalance of hackme is ', hackmecoin.getBalanceOfAddress('hackme-address'));
+console.log('\nBalance of hackme is ', hackmecoin.getBalanceOfAddress(myWalletAddress));
